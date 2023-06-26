3487bfb2da858010b65907415499ba623fe01ab4
Rohith Poyyeri <rohithart@yahoo.com>
2023-06-26 19:33:32 +1000

    feat: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppConfig } from './configs/app.config';
+import { TodoComponent } from './components/todo/todo.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: AppConfig.routes.todo, component: TodoComponent, pathMatch: 'full' },
+  { path: AppConfig.routes.error404, component: NotFoundComponent },
+  { path: '**', redirectTo: '/' + AppConfig.routes.error404 }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { APP_CONFIG, AppConfig } from './configs/app.config';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from './shared/modules/shared.module';
+import { MaterialModule } from './shared/modules/material.module';
+import { TodoModule } from './components/todo/todo.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    SharedModule,
+    TodoModule,
   ],
-  providers: [],
+  providers: [
+    { provide: APP_CONFIG, useValue: AppConfig },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
